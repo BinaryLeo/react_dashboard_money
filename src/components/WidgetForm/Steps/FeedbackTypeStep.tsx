7530a8d6c4f8267421c43736daf70fa4be723344
binaryleo <leomouracss@fastmail.com>
2022-06-20 21:56:58 -0300

    chore: feedback widget title dimensions fix

--- src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackTypeStep.tsx
@@ -9,7 +9,7 @@ export function FeedbackTypeStep({ onFeedbackTypeChanged }: FeedbackTypeStepProp
     return (
         <>
             <header>
-                <span className="text-xl leading-6">Please give us your feedback!</span>
+                <span className="text-base leading-6">Please give us your feedback!</span>
                 <CloseButton />
             </header>
             <div className="flex py-8 gap-2 w-full">
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -39,12 +39,11 @@ export const GlobalStyle = createGlobalStyle`
         opacity: 0.6;
         cursor: not-allowed;
     }
- `;
-
-/*  body, input, textarea, button{
-    font-family: 'Poppins', sans-serif;
+    body, input, textarea, button{
+        font-family: 'Poppins', sans-serif;
     font-weight:400;
-}
-h1, h2, h3, h4, h5, h6, strong{
-   font-weight:600; 
-} */
+    }
+    h1, h2, h3, h4, h5, h6, strong{
+     font-weight:600; 
+} 
+ `;
