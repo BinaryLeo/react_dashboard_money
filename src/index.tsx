962e433be30668fecb0cf5e052e4277c69afbee5
binaryleo <leomouracss@fastmail.com>
2022-06-21 23:18:23 -0300

    chore: add miragejs settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from "react-dom/client";
+import {createServer} from "miragejs"
 import { App } from './App';
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const date = new Date();
+const dateOptions: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: '2-digit',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit'
+};
+createServer({
+  routes(){
+    this.namespace ='api';
+    this.get('/transactions', ()=>{
+      return[
+        {
+          id:1,
+          title:'Transaction 1',
+          amount:400,
+          type:'deposit',
+          category:'Food',
+          createDate : date.toLocaleDateString('en-CA', dateOptions)
+        }
+      ]
+    })
+  }
+})
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
+root.render(<App />);
